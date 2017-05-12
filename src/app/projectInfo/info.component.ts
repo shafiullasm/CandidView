6e919d823b075a3de7ebb16d9ab924155170c9aa
MaheshBabu25 <Mahesh.Babu@mphasis.com>
2017-05-12 10:37:54 +0530

    sample view implemented

--- src/app/projectInfo/info.component.ts
+++ src/app/projectInfo/info.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Info } from './info';
 import { InfoService } from './info.service';
+import {IGridData} from './info' ;
+import {IColumn} from './info' ;
+//import {IRow} from './info' ;
+import {IData} from './info' ;
+import {ICell} from './info' ;
 
 @Component({
   selector: 'dash-info',
   templateUrl: './info.component.html',
+  styleUrls: ['./style.css'], 
+            
 })
 export class InfoComponent implements OnInit {
   info: Info[];
@@ -14,6 +21,75 @@ export class InfoComponent implements OnInit {
     this.getProjectDetails();
 
   }
+ gridData:IGridData = 
+  {
+    Columns: [{
+      Display:"Project"
+    },
+    {
+      Display :"Project Manager"
+    },
+    {
+      Display:"Scope"
+    },
+    {
+      Display:"Quality"
+    },
+    {
+      Display:"Cost"
+    },
+    {
+      Display:"Resource"
+    },
+    ],
+
+    Rows:[{
+       // Data: [{
+            Cells:[{
+                Display:"RWT"               
+              },
+              {
+                Display:"Kady"                
+              },
+              {
+                Background:"Yellow"                
+              },
+              {
+                Background:"Green"                
+              },
+              {
+                Background:"Red"                
+              },
+              {
+                Background:"Green"                
+              }]
+         // }]
+       },
+       {
+       // Data: [{
+            Cells:[{
+                Display:"SAC"                
+              },
+              {
+                Display:"Anuradha",                
+              },
+              {
+                Background:"Green"                
+              },
+              {
+                Background:"Red"                
+              },
+              {
+                Background:"Green"                
+              },
+              {
+                Background:"Yellow"                
+              }]
+         // }]
+       }
+       ]
+    };
+
 
   getProjectDetails(): void {
     this.infoService.getProjectDetails().subscribe(x => this.info = x);
--- src/app/projectInfo/info.ts
+++ src/app/projectInfo/info.ts
@@ -2,3 +2,27 @@ export class Info {
   id: any;
   name: string;
 }
+
+export interface IGridData {
+
+  Columns :IColumn[] ;
+  Rows : IData[];
+}
+
+export interface IColumn {
+  Display:string;
+}
+
+//export interface IRow {
+
+  //Data : IData[] ;
+//}
+
+export interface IData {
+  Cells:ICell[];
+}
+export interface ICell {
+  Display ?: string ;
+  Background?: string ;
+  Editable?: Boolean;
+}
