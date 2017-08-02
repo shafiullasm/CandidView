d9be668cfa5e492a51ed4f15fdcbd8e7f615344b
shubhabr01 <shubha.br01@mphasis.com>
2017-08-02 13:14:33 +0530

    Minor business rule change

--- src/app/project-status/project-status.component.ts
+++ src/app/project-status/project-status.component.ts
@@ -61,7 +61,7 @@ export class ProjectStatusComponent implements OnInit {
         scheduleRemarks: scheduleData.remarks,
         qualityRemarks: qualityData.remarks,
         qualityEngineeringRemarks: qualityEngineeringPractice.remarks,
-        resourceRemarks: resData.color
+        resourceRemarks: resData.remarks
       };
       let projStatus: IProjectStatus = new ProjectStatus();
       projStatus = element;
@@ -78,30 +78,31 @@ export class ProjectStatusComponent implements OnInit {
       Colors[0] = IEnumColors.Green;
     } else if (scopeValues.backlogPresent === 'P') {
       Colors[0] = IEnumColors.Yellow;
-      Remarks.push(this.formRemarks("Product backlog insufficient for next 3 sprints"));
+      Remarks.push(this.formRemarks('Product backlog insufficient for next 3 sprints'));
     } else {
       Colors[0] = IEnumColors.Red;
-      Remarks.push(this.formRemarks("Product backlog insufficient for next 2 sprints"));
+      Remarks.push(this.formRemarks('Product backlog insufficient for next 2 sprints'));
     }
 
     if (scopeValues.stories.toUpperCase() === 'Y' || scopeValues.stories.toUpperCase() === 'YES') {
       Colors[1] = IEnumColors.Green;
     } else if (scopeValues.stories === 'P') {
       Colors[1] = IEnumColors.Yellow;
-      Remarks.push(this.formRemarks("Insufficient groomed stories for next sprint / stories have more than 5 points"));
+      Remarks.push(this.formRemarks('Insufficient groomed stories for next sprint / stories have more than 5 points'));
     } else {
       Colors[1] = IEnumColors.Red;
-      Remarks.push(this.formRemarks("No groomed stories for next sprint / stories have more than 5 points"));
+      Remarks.push(this.formRemarks('No groomed stories for next sprint / stories have more than 5 points'));
     }
 
     if (scopeValues.developmentDependencies.toUpperCase() === 'Y' || scopeValues.developmentDependencies.toUpperCase() === 'YES') {
       Colors[2] = IEnumColors.Green;
     } else if (scopeValues.developmentDependencies.toUpperCase() === 'P') {
       Colors[2] = IEnumColors.Yellow;
-      Remarks.push(this.formRemarks("Dependencies identified partially / vague or no commitment on timelines from the dependent team on delivery"));
+      Remarks.push(this.formRemarks('Dependencies identified partially / vague or no commitment on timelines' +
+        'from the dependent team on delivery'));
     } else {
       Colors[2] = IEnumColors.Red;
-      Remarks.push(this.formRemarks("No dependencies identified /  no commitment on timelines from the dependent team on delivery"));
+      Remarks.push(this.formRemarks('No dependencies identified /  no commitment on timelines from the dependent team on delivery'));
     }
 
     if (scopeValues.tgoDesign.toUpperCase() === 'Y' || scopeValues.tgoDesign.toUpperCase() === 'YES'
@@ -112,10 +113,11 @@ export class ProjectStatusComponent implements OnInit {
       && (scopeValues.noOfDaysFromStartDate >= 15 &&
         scopeValues.noOfDaysFromStartDate < 30)) {
       Colors[3] = IEnumColors.Yellow;
-      Remarks.push(this.formRemarks("TGO Design pending for Release #" + release + " and #" + scopeValues.noOfDaysFromStartDate + " days over from project started"));
+      Remarks.push(this.formRemarks('TGO Design pending for Release #' + release + ' and #' +
+        scopeValues.noOfDaysFromStartDate + ' days over from project started'));
     } else {
       Colors[3] = IEnumColors.Red;
-      Remarks.push(this.formRemarks("TGO Design pending for Release #" + release));
+      Remarks.push(this.formRemarks('TGO Design pending for Release #' + release));
     }
 
     if (scopeValues.tgoConstruction.toUpperCase() === 'Y' || scopeValues.tgoConstruction.toUpperCase() === 'YES'
@@ -125,14 +127,15 @@ export class ProjectStatusComponent implements OnInit {
       && (scopeValues.noOfDaysFromCodeFreezeDate > 30 &&
         scopeValues.noOfDaysFromCodeFreezeDate <= 45)) {
       Colors[4] = IEnumColors.Yellow;
-      Remarks.push(this.formRemarks("TGO Construction pending for Release #" + release + " and #" + scopeValues.noOfDaysFromCodeFreezeDate + " more days left for code freeze"));
+      Remarks.push(this.formRemarks('TGO Construction pending for Release #' + release + ' and #' +
+        scopeValues.noOfDaysFromCodeFreezeDate + ' more days left for code freeze'));
     } else {
       Colors[4] = IEnumColors.Red;
-      Remarks.push(this.formRemarks("TGO Design pending for Release #" + release));
+      Remarks.push(this.formRemarks('TGO Design pending for Release #' + release));
     }
 
     let bgcolor = this.overAllColor(Colors);
-    let scopeData = { "color": bgcolor, "remarks": Remarks };
+    let scopeData = { 'color': bgcolor, 'remarks': Remarks };
     return scopeData;
   }
 
@@ -144,12 +147,12 @@ export class ProjectStatusComponent implements OnInit {
       bgcolor = IEnumColors.Green;
     } else if (value >= -1 && value < 0) {
       bgcolor = IEnumColors.Yellow;
-      Remarks.push(this.formRemarks("1 sprint short to meet schedule"));
+      Remarks.push(this.formRemarks('1 sprint short to meet schedule'));
     } else {
       bgcolor = IEnumColors.Red;
-      Remarks.push(this.formRemarks("More than 1 sprint short to meet schedule"));
+      Remarks.push(this.formRemarks('More than 1 sprint short to meet schedule'));
     }
-    let scheduleData = { "color": bgcolor, "remarks": Remarks };
+    let scheduleData = { 'color': bgcolor, 'remarks': Remarks };
     return scheduleData;
   }
 
@@ -162,42 +165,42 @@ export class ProjectStatusComponent implements OnInit {
       Colors[0] = IEnumColors.Green;
     } else if (qualityValues.requirementTestCoverage >= -0.1 && qualityValues.requirementTestCoverage < 0) {
       Colors[0] = IEnumColors.Yellow;
-      Remarks.push(this.formRemarks("Few requirements are yet to tested"));
+      Remarks.push(this.formRemarks('Few requirements are yet to tested'));
     } else {
       Colors[0] = IEnumColors.Red;
-      Remarks.push(this.formRemarks("TestRequirementCovergae is low, many requirement yet to be tested"));
+      Remarks.push(this.formRemarks('TestRequirementCovergae is low, many requirement yet to be tested'));
     }
 
     // if (qualityValues.averageLeadTime <= 0.2) {
     //   Colors[1] = IEnumColors.Green;
     // } else if (qualityValues.averageLeadTime > 0.2 && qualityValues.averageLeadTime < 0.5) {
     //   Colors[1] = IEnumColors.Yellow;
-    //   Remarks.push(this.formRemarks(""));
+    //   Remarks.push(this.formRemarks(''));
     // } else {
     //   Colors[1] = IEnumColors.Red;
-    //   Remarks.push(this.formRemarks(""));
+    //   Remarks.push(this.formRemarks(''));
     // }
 
     if (qualityValues.defectLeakageQA <= 0.2) {
       Colors[1] = IEnumColors.Green;
     } else if (qualityValues.defectLeakageQA > 0.2 && qualityValues.defectLeakageQA < 0.5) {
       Colors[1] = IEnumColors.Yellow;
-      Remarks.push(this.formRemarks("QA defect leakage is moderatly higher then the desired level"));
+      Remarks.push(this.formRemarks('QA defect leakage is moderatly higher then the desired level'));
     } else {
       Colors[1] = IEnumColors.Red;
-      Remarks.push(this.formRemarks("QA defect leakage is higher then the desired level"));
+      Remarks.push(this.formRemarks('QA defect leakage is higher then the desired level'));
     }
 
-    if (qualityValues.productionDefect == 0) {
+    if (qualityValues.productionDefect === 0) {
       Colors[2] = IEnumColors.Green;
     } else {
       Colors[2] = IEnumColors.Red;
-      Remarks.push(this.formRemarks("Prod defect is greater than 0 - #" + qualityValues.productionDefect));
+      Remarks.push(this.formRemarks('Prod defect is greater than 0 - #' + qualityValues.productionDefect));
     }
 
     bgColor = this.overAllColor(Colors);
 
-    let qtyData = { "color": bgColor, "remarks": Remarks };
+    let qtyData = { 'color': bgColor, 'remarks': Remarks };
     return qtyData;
   }
 
@@ -210,28 +213,28 @@ export class ProjectStatusComponent implements OnInit {
       Colors[0] = IEnumColors.Green;
     } else if (qualityEngineering.tddCoverage >= 70 && qualityEngineering.tddCoverage < 80) {
       Colors[0] = IEnumColors.Yellow;
-      Remarks.push(this.formRemarks("TDD coverage is between 70% and 80%: " + qualityEngineering.tddCoverage + "%"));
+      Remarks.push(this.formRemarks('TDD coverage is between 70% and 80%: ' + qualityEngineering.tddCoverage + '%'));
     } else {
       Colors[0] = IEnumColors.Red;
-      Remarks.push(this.formRemarks("TDD coverage is less then 70%: " + qualityEngineering.tddCoverage + "%"));
+      Remarks.push(this.formRemarks('TDD coverage is less then 70%: ' + qualityEngineering.tddCoverage + '%'));
     }
-    if (qualityEngineering.bddCoverage >= 80 || qualityEngineering.bddCoverage===0) {
+    if (qualityEngineering.bddCoverage >= 80 || qualityEngineering.bddCoverage === 0) {
       Colors[1] = IEnumColors.Green;
     } else if (qualityEngineering.bddCoverage >= 65 && qualityEngineering.bddCoverage < 80) {
       Colors[1] = IEnumColors.Yellow;
-      Remarks.push(this.formRemarks("BDD coverage is between 65% and 80%: " + qualityEngineering.bddCoverage + "%"));
+      Remarks.push(this.formRemarks('BDD coverage is between 65% and 80%: ' + qualityEngineering.bddCoverage + '%'));
     } else {
       Colors[1] = IEnumColors.Red;
-      Remarks.push(this.formRemarks("BDD coverage is less than 65%: " + qualityEngineering.bddCoverage + "%"));
+      Remarks.push(this.formRemarks('BDD coverage is less than 65%: ' + qualityEngineering.bddCoverage + '%'));
     }
     if (qualityEngineering.mvpAdoption >= 2 || qualityEngineering.mvpAdoption === 0) {
       Colors[2] = IEnumColors.Green;
     } else if (qualityEngineering.mvpAdoption = 1) {
       Colors[2] = IEnumColors.Yellow;
-      Remarks.push(this.formRemarks("One production release in last 6 months"));
+      Remarks.push(this.formRemarks('One production release in last 6 months'));
     } else {
       Colors[2] = IEnumColors.Red;
-      Remarks.push(this.formRemarks("No production release in last 6 months"));
+      Remarks.push(this.formRemarks('No production release in last 6 months'));
     }
     if (qualityEngineering.codeReviewDev.catastrophic === 0 &&
       qualityEngineering.codeReviewDev.majorDefectsWithoutWorkaround === 0 &&
@@ -242,12 +245,12 @@ export class ProjectStatusComponent implements OnInit {
       qualityEngineering.codeReviewDev.majorDefectsWithoutWorkaround === 0 &&
       qualityEngineering.codeReviewDev.majorDefectsWithWorkaround < 5) {
       Colors[3] = IEnumColors.Yellow;
-      Remarks.push(this.formRemarks("Minor defects identified during  Dev code review"));
+      Remarks.push(this.formRemarks('Minor defects identified during  Dev code review'));
     } else if (qualityEngineering.codeReviewDev.catastrophic > 0 ||
       qualityEngineering.codeReviewDev.majorDefectsWithoutWorkaround > 0 ||
       qualityEngineering.codeReviewDev.majorDefectsWithWorkaround > 5) {
       Colors[3] = IEnumColors.Red;
-      Remarks.push(this.formRemarks("Major defects identified during  Dev code review"));
+      Remarks.push(this.formRemarks('Major defects identified during  Dev code review'));
     }
     if (qualityEngineering.codeReviewQA.catastrophic === 0 &&
       qualityEngineering.codeReviewQA.majorDefectsWithoutWorkaround === 0 &&
@@ -258,28 +261,28 @@ export class ProjectStatusComponent implements OnInit {
       qualityEngineering.codeReviewQA.majorDefectsWithoutWorkaround === 0 &&
       qualityEngineering.codeReviewQA.majorDefectsWithWorkaround < 5) {
       Colors[4] = IEnumColors.Yellow;
-      Remarks.push(this.formRemarks("Minor defects identified during  QA code review"));
+      Remarks.push(this.formRemarks('Minor defects identified during  QA code review'));
     } else if (qualityEngineering.codeReviewQA.catastrophic > 0 ||
       qualityEngineering.codeReviewQA.majorDefectsWithoutWorkaround > 0 ||
       qualityEngineering.codeReviewQA.majorDefectsWithWorkaround > 5) {
       Colors[3] = IEnumColors.Red;
-      Remarks.push(this.formRemarks("Major defects identified during  QA code review"));
+      Remarks.push(this.formRemarks('Major defects identified during  QA code review'));
     }
     if (qualityEngineering.maintainabilityIndex >= 60 || qualityEngineering.maintainabilityIndex === 0) {
       Colors[5] = IEnumColors.Green;
     } else {
       Colors[5] = IEnumColors.Red;
-      Remarks.push(this.formRemarks("MI less than 60"));
+      Remarks.push(this.formRemarks('MI less than 60'));
     }
     if (qualityEngineering.cyclomaticComplexity <= 15 || qualityEngineering.cyclomaticComplexity === 0) {
       Colors[6] = IEnumColors.Green;
     } else {
       Colors[6] = IEnumColors.Red;
-      Remarks.push(this.formRemarks("CC greater than 15"));
+      Remarks.push(this.formRemarks('CC greater than 15'));
     }
     bgColor = this.overAllColor(Colors);
 
-    let qtyData = { "color": bgColor, "remarks": Remarks };
+    let qtyData = { 'color': bgColor, 'remarks': Remarks };
     return qtyData;
   }
 
@@ -290,25 +293,25 @@ export class ProjectStatusComponent implements OnInit {
 
     if (attrition === 0) {
       Colors[0] = IEnumColors.Green;
-    } else if (attrition == 1) {
+    } else if (attrition === 1) {
       Colors[0] = IEnumColors.Yellow;
-      Remarks.push(this.formRemarks("Attrition of resources is equal to 1"));
+      Remarks.push(this.formRemarks('Attrition of resources is equal to 1'));
     } else {
       Colors[0] = IEnumColors.Red;
-      Remarks.push(this.formRemarks("Attrition of resources is greater than 1"));
+      Remarks.push(this.formRemarks('Attrition of resources is greater than 1'));
     }
     if (availabilityofResource.toUpperCase() === 'YES') {
       Colors[1] = IEnumColors.Green;
     } else if (availabilityofResource.toUpperCase() === 'PARTIAL') {
       Colors[1] = IEnumColors.Yellow;
-      Remarks.push(this.formRemarks("Partial availability of Resource with Required Skillset"));
+      Remarks.push(this.formRemarks('Partial availability of Resource with Required Skillset'));
     } else {
       Colors[1] = IEnumColors.Red;
-      Remarks.push(this.formRemarks("No available Resource with Required Skillset"));
+      Remarks.push(this.formRemarks('No available Resource with Required Skillset'));
     }
     bgColor = this.overAllColor(Colors);
 
-    let resData = { "color": bgColor, "remarks": Remarks };
+    let resData = { 'color': bgColor, 'remarks': Remarks };
     return resData;
   }
 
